ea31b9744376d4ebcf8fd8346e74fe01cba208a7
ebellob <ebellob@upv.es>
2025-12-29 14:25:39 +0100

    aa

--- check_tensor_shapes.py
+++ check_tensor_shapes.py
@@ -1,11 +1,11 @@
 import torch
 import os
-
+from tqdm import tqdm
 DIR = "data/rois"
 
 for dirpath, dirnames, filenames in os.walk(DIR):
     for speaker in dirnames:
-        for file in os.listdir(os.path.join(dirpath, speaker)):
+        for file in tqdm(os.listdir(os.path.join(dirpath, speaker)), desc=f"Checking tensors for speaker {speaker}"):
             if file.endswith(".pt"):
                 tensor = torch.load(os.path.join(dirpath, speaker, file))
                 if list(tensor.shape) != [tensor.shape[0], 96, 96]:
