d062d4daf8858502a8325da28bd1e3de80565faf
ebellob <ebellob@upv.es>
2026-01-04 09:46:57 +0100

    augmented lr

--- src/train.py
+++ src/train.py
@@ -209,7 +209,7 @@ def train(
             
 
     optimizer = AdamW(
-        [{"params": base_params, "lr" : learning_rate}, {"params": visual_params, "lr": learning_rate * 2}],
+        [{"params": base_params, "lr" : learning_rate}, {"params": visual_params, "lr": learning_rate * 10}],
         weight_decay=weight_decay,
     )
 
