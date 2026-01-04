33beaed0a42c4ac8cc4db1dcba910f0efb196413
ebellob <ebellob@upv.es>
2026-01-04 08:49:39 +0100

    remove scaling

--- src/voxcpm/model/voxcpm.py
+++ src/voxcpm/model/voxcpm.py
@@ -945,10 +945,9 @@ class VoxCPMModel(nn.Module):
                 # Get visual condition for current step i
                 if i < visual_cond_seq.shape[1]:
                     curr_vis = visual_cond_seq[:, i, :] # [B, H]
-                    dit_hidden += curr_vis * 2  # scale visual condition
+                    dit_hidden += curr_vis  # scale visual condition
 
-                    base = (dit_hidden_1 + dit_hidden_2).abs().mean().item()
-                    vis  = curr_vis.abs().mean().item()
+
                     # print("base:", base, "vis:", vis, "vis/base:", vis/(base+1e-8))
             
                     # print(f"Magnitudes:\n LM to DIT: {dit_hidden_1.abs().mean().item():.4f}, Residual to DIT: {dit_hidden_2.abs().mean().item():.4f}, Visual Cond: {curr_vis.abs().mean().item():.4f}")
